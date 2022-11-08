34f25afa3d22b6fd52e9ea6dbba5b0d42985cbc7
Vision84 <electri.squad3@gmail.com>
2022-11-08 10:59:56 -0500

    Implemented a SPA
    Implemented a simple SPA to change the page using a single JavaScript function. It is coded in a way that allows for easy addition of new pages.

---
+++ index.js
@@ -0,0 +1,22 @@
+let activePage = "home";
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadPage(activePage);
+});
+
+function loadPage(activePage) {
+    activePage = activePage;
+    let contents = document.querySelectorAll(".main-content");
+
+    for (let page = 0; page < contents.length; page++) {
+        if (contents[page].id === activePage) {
+            contents[page].style.display = "block";
+        } else {
+            contents[page].style.display = "none";
+        }
+    }
+
+    if (document.querySelector(".main-navigation-toggle").checked) {
+        document.querySelector(".main-navigation-toggle").checked = false;
+    }
+}
