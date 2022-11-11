d8f1e46891c0d157d88ceb0cfc7d8e16791b494b
Vision84 <electri.squad3@gmail.com>
2022-11-11 10:38:02 -0500

    Fixed fade-in and fade-out
    Fixed an issue with CSS and JS where fade-in and fade-out animations were not working. I created a workaround using JS timers that would essentially do the same thing. I had to clutter the initial DOMContentLoaded Event in order to achieve the animation that I wanted.

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
 let currentPage = "home";
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    loadPage(currentPage);
-});
-
-function loadPage(activePage) {
     const contents = document.querySelectorAll(".main-content");
-
+    
     for (let page = 0; page < contents.length; page++) {
-        if (contents[page].id === activePage) {
-            contents[page].style.display = "block";
-        } else {
+        if (contents[page].id !== currentPage) {
             contents[page].style.display = "none";
         }
-
-        currentPage = activePage;
     }
+
+    for (let page = 0; page < contents.length; page++) {
+        if (contents[page].id === currentPage) {
+            unfade(contents[page]);
+        }
+    }
+});
+
+async function loadPage(activePage) {
+    const contents = document.querySelectorAll(".main-content");
 
     if (document.querySelector(".main-navigation-toggle").checked) {
         document.querySelector(".main-navigation-toggle").checked = false;
     }
+
+    for (let page = 0; page < contents.length; page++) {
+        
+        if (contents[page].id !== activePage) {
+            await fade(contents[page]);
+        }
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 400));
+
+    for (let page = 0; page < contents.length; page++) {
+        
+        if (contents[page].id === activePage) {
+            await unfade(contents[page]);
+        }
+        currentPage = activePage;
+    }
+
+}
+
+async function unfade(element) {
+    var op = 0.1;
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 1);
+}
+
+async function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 1);
 }
